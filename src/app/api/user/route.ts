69995c214f991dab2960d909bdcd2a8f94fd2764
Arman Monajemi <watbut67@gmail.com>
2025-10-16 12:12:57 +0330

    prevent duplicate users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -22,18 +22,23 @@ export const GET = async (req: Request) => {
 
 export const POST = async (req: Request) => {
     await connectToDatabase();
-    
+
     try {
         const body = await req.json();
         const user = createUserSchema.parse(body);
 
+        const foundUser = userRepository ? await userRepository.findByUsername(user.username) : null;
+
         if (!userRepository) {
             return responseJson("User repository not available!");
+        }
+        if (foundUser) {
+            return responseJson("User with the given username already exists!");
         }
 
         const created = await userRepository.create(user);
         return responseJson('ok', 201);
-        
+
     } catch (err: any) {    // eslint-disable-line
         const message = err?.message || "Invalid request";
         const status = err?.name === "ZodError" ? 400 : 400;

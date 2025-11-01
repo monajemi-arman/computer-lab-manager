bca14acb7981930e611e2276136c3160ef255dfa
Arman Monajemi <watbut67@gmail.com>
2025-11-01 12:24:58 +0330

    move server action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,6 @@
-"use server"
+"use server";
 
-import { signIn } from "@/auth"
+import { auth, signIn } from "@/auth"
 import { isRedirectError } from "next/dist/client/components/redirect-error"
 import { CredentialsSignin } from "next-auth"
 
@@ -18,4 +18,17 @@ export async function loginAction(state: any, formData: FormData) { // eslint-di
     
     return { error: "An error occurred during login" }
   }
+}
+
+export const getSession = async () => {
+  return await auth();
+}
+
+export const getIsAdmin = async () => {
+  const session = await getSession();
+
+  if (session && session.user && session.user.role == 'admin')
+    return true;
+  
+  return false;
 }

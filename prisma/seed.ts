bbf4ffb93be84c157fdb9b7e553f44a54f88aa5a
Arman Monajemi <watbut67@gmail.com>
2025-11-12 23:58:15 +0330

    seed prisma after npm install

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prismaClient";
+
+async function main() {
+    await prisma.$connect();
+
+    await prisma.playbook.createMany({
+        data: [
+            { name: 'Test', filename: 'test.yml', description: 'Check if playbooks can run' }
+        ]
+    })
+}
+
+main()
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })

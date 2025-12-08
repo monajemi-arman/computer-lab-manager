8252f3bea6f1ed3b36673142a09487d60c77c0c7
Arman Monajemi <watbut67@gmail.com>
2025-12-08 10:57:59 +0330

    fix forgot default dashboard view only on default page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
             />
           </div>
         </header>
-        <DefaultDashboardView />
+        {(hash == '' || hash =='#') && <DefaultDashboardView />}
         {
           // Admin Views
         }
